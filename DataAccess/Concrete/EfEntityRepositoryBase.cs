e9819bef21ce97055203d1f571a42c671224d911
Serhat O <developer.serhat@hotmail.com>
2021-04-08 17:36:24 +0300

    Veritabanı ve class bağlantıları. Temel veritabanı işlemleri

---
+++ DataAccess/Concrete/EfEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete  //Bu class ekleme,silme,güncelleme ve listeleme(temel işlemleri) yapar. 
+{
+    class EfEntityRepositoryBase<T, TContext> : IEntityRepository<T>
+        where T : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(T entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<T>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<T> GetList(Expression<Func<T, bool>> filter = null)
+        {
+            throw new NotImplementedException(); using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<T>().ToList()
+                    : context.Set<T>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/TatliSozlukContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    class TatliSozlukContext : DbContext
+    {
+        // bu class veritabanı ile proje class'larını bağlar
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TatliSozluk1;Trusted_Connection=true");
+        }
+
+        public DbSet<Uye> Uye { get; set; }
+        public DbSet<Konu> Konu { get; set; }
+        public DbSet<Baslik> Baslik { get; set; }
+        public DbSet<Yorum> Yorum { get; set; }
+    }
+}
